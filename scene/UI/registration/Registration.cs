9147d5232899e1dc614ac5b026d4fa874ae03323
Vladislav <vladislav.a.koltovicha@gmail.com>
2024-11-27 23:07:07 +0300

    feat: add verification by email

--- scene/UI/registration/Registration.cs
+++ scene/UI/registration/Registration.cs
@@ -1,20 +1,27 @@
 using Godot;
 using System;
 using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Mail;
 
 public partial class Registration : CanvasLayer
 {
+	private LineEdit mailEdit;
+	private LineEdit loginEdit;
+	private LineEdit pswEdit;
+	private LineEdit repPswEdit;
+
 	public override void _Ready()
 	{
-		var mailEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/mailEdit");
-		var loginEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/LoginEdit");
-		var pswEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/HBoxContainer4/PswEdit");
-		var repPswEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/HBoxContainer/RepPswEdit");
+		mailEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/mailEdit");
+		loginEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/LoginEdit");
+		pswEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/HBoxContainer4/PswEdit");
+		repPswEdit = GetNode<LineEdit>("MarginContainer/VBoxContainer/HBoxContainer/RepPswEdit");
 		mailEdit.TextChanged += MailFilter;
 		loginEdit.TextChanged += LoginFilter;
 		pswEdit.TextChanged += PswFilter;
 		repPswEdit.TextChanged += RepPswFilter;
-	}
+    }
 
 	public override void _Process(double delta)
 	{
@@ -49,8 +56,7 @@ public partial class Registration : CanvasLayer
 	private void MailFilter(string newText)
 	{
 		var Edit = GetNode<LineEdit>("MarginContainer/VBoxContainer/mailEdit");
-		
-		Regex NumberRegex = new Regex(@"^[-\w.]+@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,4}$");
+		Regex NumberRegex = new Regex(@"[^-A-Za-z0-9.@_]");
 		int cursorPosition = Edit.CaretColumn;
         string filteredText = NumberRegex.Replace(newText, "");
 
@@ -106,4 +112,42 @@ public partial class Registration : CanvasLayer
 
 		Edit.CaretColumn = cursorPosition;
 	}
+
+	private void SendsendConfirmationMessages(string toEmail)
+	{
+		GD.Print(mailEdit.Text);
+		try
+		{
+			SmtpClient smtpClient = new SmtpClient("smtp.mail.ru")
+			{
+				Port = 587,
+				Credentials = new NetworkCredential("alissentertainment@mail.ru", "ate5yzgzyP4bMB7KfJ0K"),
+				EnableSsl = true,
+			};
+
+			MailMessage mail = new MailMessage
+			{
+				From = new MailAddress("alissentertainment@mail.ru", "Admin"),
+				Subject = "Потвержденние регистрации",
+				Body = Random.Shared.Next(100000, 1000000).ToString(),
+				IsBodyHtml = false 
+			};
+
+			mail.To.Add(toEmail);
+			smtpClient.Send(mail);
+			GD.Print("Email успешно отправлен!");
+		}
+		catch (Exception ex)
+		{
+			GD.PrintErr($"Ошибка при отправке email: {ex.Message}");
+		}
+	}
+
+	private void CreateAccount()
+	{
+
+		string toEmail = mailEdit.Text;
+		SendsendConfirmationMessages(toEmail);
+	}
+
 }
