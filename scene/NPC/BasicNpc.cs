7297b9b7e4914b53b10caff7bb560dbac7a58c43
Vladislav <vladislav.a.koltovicha@gmail.com>
2024-12-18 21:32:55 +0300

    feat: add dialogueScenes and dialogueScript

--- scene/NPC/BasicNpc.cs
+++ scene/NPC/BasicNpc.cs
@@ -3,13 +3,16 @@ using System;
 
 public partial class BasicNpc : Node2D
 {
+    PackedScene balloonScene;
     protected Control interactableLable;
     InteractableComponent interactableComponent;
+    bool isRange = false;
 
     public override void _Ready()
     {
         interactableComponent = GetNode<InteractableComponent>("InteractableComponent");
         interactableLable = GetNode<Control>("InteractableLabelComponent");
+        balloonScene = GD.Load<PackedScene>("res://dialogue/GameDialogueBalloon.tscn");
 
         interactableComponent.InteractableActivated += OnInteractableActivated;
         interactableComponent.InteractableDeactivated += OnInteractableDeactivated;
@@ -20,10 +23,22 @@ public partial class BasicNpc : Node2D
     private void OnInteractableActivated()
     {
         interactableLable.Show();
+        isRange = true;
     }
 
     private void OnInteractableDeactivated()
     {
         interactableLable.Hide();
+        isRange = false;
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (Input.IsActionJustPressed("Interaction") && isRange)
+        {
+            BaseGameDialogueBalloon balloon = (BaseGameDialogueBalloon)balloonScene.Instantiate();
+            GetTree().CurrentScene.AddChild(balloon);
+            balloon.Start(GD.Load("res://dialogue/conversations/GloriaStuart.dialogue"), "start");
+        }
     }
 }
